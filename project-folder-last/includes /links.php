8aacb414e00a88da31ed4d3e14a595f7228c026a
shrooq220 <shroogcy@hotmail.com>
2025-05-14 17:27:32 +0300

    Update links.php

--- project-folder-last/includes /links.php
+++ project-folder-last/includes /links.php
@@ -5,7 +5,7 @@
         <li><a href="../pages/services.php">Services</a></li>
         <li><a href="../pages/quizzes.php">Quizzes</a></li>
         <li><a href="../pages/quran_tafsir.php">Quran & Tafsir</a></li>
-        <li><a href="../pages/prayer_schedule.php">Prayer Schedule</a></li>
+        <li><a href="../pages/schedule.php">Prayer Schedule</a></li>
         <li><a href="../pages/videos.php">Islamic Videos</a></li>
         <li><a href="../pages/contact.php">Contact</a></li>
         <li><a href="../pages/feedback.php">Feedback</a></li>
