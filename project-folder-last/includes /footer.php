b8b5fd1f74cbb7b95dee3200359cfc142e3f57f7
shrooq220 <shroogcy@hotmail.com>
2025-05-14 16:42:22 +0300

    Update footer.php

--- project-folder-last/includes /footer.php
+++ project-folder-last/includes /footer.php
@@ -1,1 +1,15 @@
-
+<div id="footer">
+    <div class="footer-left">
+        <p>© <?php echo date("Y"); ?> 2025 Al-Noor. All rights reserved.</p>
+    </div>
+    <div class="footer-right">
+        <address>
+            Al-Noor Islamic Center<br>
+            123 Faith Street, Jeddah, Saudi Arabia<br>
+            Email: contact@alnoor.com<br>
+            Phone: +123-456-7890
+        </address>
+    </div>
+</div>
+</body>
+</html>
