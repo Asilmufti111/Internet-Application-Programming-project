9db412bcd9cbc60e51321e76ddc76c36f2a3418c
shrooq220 <shroogcy@hotmail.com>
2025-05-15 12:48:04 +0300

    Update videos.php

--- project-folder-last/pages/videos.php
+++ project-folder-last/pages/videos.php
@@ -1,49 +1,63 @@
-<?php
-// Define page title
-$page_title = "Islamic Videos - Al-Noor";
+<!-- 
+    Names: Asil Mufti, Hayat Alzahrani, Shrooq Kaabi
+    IDs: 2106122, 2105101, 2205003
+    Section: IAR
+    Date: 20-05-2025
+-->
 
-// Include header and links
-require_once '../includes/header.php';
-require_once '../includes/links.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta Information -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Islamic Videos - Al-Noor</title>
+    <link rel="stylesheet" href="../global/main.css">
+</head>
+<body>
 
-<!-- Main Content Section -->
-<main>
-    <section class="content">
-        <h2>Featured Islamic Videos</h2>
+    <!-- Include Header -->
+    <?php include('../includes/header.php'); ?>
 
-        <div class="video-container">
-            <iframe width="560" height="315" 
-                src="https://www.youtube.com/embed/UitSEoGBvls" 
-                title="Quran Recitation by Saud Al-Shuraim" 
-                frameborder="0" 
-                allowfullscreen>
-            </iframe>
-            <p>Beautiful recitation of Surah Al-Baqarah by Saud Al-Shuraim.</p>
-        </div>
+    <!-- Include Navigation -->
+    <?php include('../includes/links.php'); ?>
 
-        <div class="video-container">
-            <iframe width="560" height="315" 
-                src="https://www.youtube.com/embed/Cdu6H8wDorY" 
-                title="Hadith Explanation - Islamic Lessons" 
-                frameborder="0" 
-                allowfullscreen>
-            </iframe>
-            <p>Explanation of a key Hadith by a scholar.</p>
-        </div>
+    <!-- Main Content Section -->
+    <main>
+        <section class="content">
+            <h2>Featured Islamic Videos</h2>
 
-        <div class="video-container">
-            <iframe width="560" height="315" 
-                src="https://www.youtube.com/embed/u3DBN0cxIo8" 
-                title="The History of the Entire Islamic Civilization in 49 Minutes" 
-                frameborder="0" 
-                allowfullscreen>
-            </iframe>
-            <p>Overview of Islamic contributions to history from Islam Channel.</p>
-        </div>
-    </section>
-</main>
+            <div class="video-container">
+                <iframe width="560" height="315" 
+                    src="https://www.youtube.com/embed/UitSEoGBvls" 
+                    title="Quran Recitation by Saud Al-Shuraim" 
+                    allowfullscreen>
+                </iframe>
+                <p>Beautiful recitation of Surah Al-Baqarah by Saud Al-Shuraim.</p>
+            </div>
 
-<?php
-require_once '../includes/footer.php';
-?>
+            <div class="video-container">
+                <iframe width="560" height="315" 
+                    src="https://www.youtube.com/embed/Cdu6H8wDorY" 
+                    title="Hadith Explanation - Islamic Lessons" 
+                    allowfullscreen>
+                </iframe>
+                <p>Explanation of a key Hadith by a scholar.</p>
+            </div>
+
+            <div class="video-container">
+                <iframe width="560" height="315" 
+                    src="https://www.youtube.com/embed/u3DBN0cxIo8" 
+                    title="The History of the Entire Islamic Civilization" 
+                    allowfullscreen>
+                </iframe>
+                <p>Overview of Islamic contributions to history from Islam Channel.</p>
+            </div>
+        </section>
+    </main>
+
+    <!-- Include Footer -->
+    <?php include('../includes/footer.php'); ?>
+
+</body>
+</html>
